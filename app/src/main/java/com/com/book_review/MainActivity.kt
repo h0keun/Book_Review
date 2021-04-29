2a4cbd839efc586981e2d1973d8f0c9414257625
h0keun <dlghrms95@gmail.com>
2021-04-29 21:56:40 +0900

    Retrofit + Glide

--- app/src/main/java/com/com/book_review/MainActivity.kt
+++ app/src/main/java/com/com/book_review/MainActivity.kt
@@ -3,7 +3,10 @@ package com.com.book_review
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.com.book_review.adapter.BookAdapter
 import com.com.book_review.api.BookService
+import com.com.book_review.databinding.ActivityMainBinding
 import com.com.book_review.model.BestSellerDto
 import retrofit2.Call
 import retrofit2.Callback
@@ -12,9 +15,16 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: BookAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBookRecyclerView()
 
         val retrofit = Retrofit.Builder() // 인터페이스형식은 구현체가 아님(동작의 정의만 되어있는것) 따라서 이렇게 따로 구현체를 만들어주어야함
                 .baseUrl("https://book.interpark.com")
@@ -23,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
         val bookService = retrofit.create(BookService::class.java)
 
-        bookService.getBestSellerBooks("private")
+        bookService.getBestSellerBooks("MY_API_KEY")
                 .enqueue(object: Callback<BestSellerDto> {
 
                     override fun onResponse(call: Call<BestSellerDto>, response: Response<BestSellerDto>) {
@@ -38,6 +48,7 @@ class MainActivity : AppCompatActivity() {
                             it.books.forEach { book ->
                                 Log.d(TAG, book.toString())
                             }
+                            adapter.submitList(it.books)
                         }
                     }
 
@@ -46,6 +57,12 @@ class MainActivity : AppCompatActivity() {
                     }
 
                 })
+    }
+
+    fun initBookRecyclerView(){
+        adapter = BookAdapter()
+        binding.bookRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.bookRecyclerView.adapter = adapter
     }
 
     companion object {
